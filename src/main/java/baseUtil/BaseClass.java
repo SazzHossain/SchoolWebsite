40ab33b3a3ce6b8355fd9730d48032f0ca2fe7cf
mshossain92 <mshossain92@ymail.com>
2024-11-24 17:17:19 -0500

    day05

--- src/main/java/baseUtil/BaseClass.java
+++ src/main/java/baseUtil/BaseClass.java
@@ -2,65 +2,69 @@ package baseUtil;
 
 import java.time.Duration;
 
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.bidi.module.Browser;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import commonMethods.CommonActions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
-import pages.HomePage;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import pages.HomePage;
+import utils.Configuration;
+import static utils.IConstant.*;
 public class BaseClass {
 	
 	public WebDriver driver;
 	public HomePage homePage;
+	 public Configuration configuration;
 	
 	@BeforeMethod
 	public void starting() {
-		//1st way
-		//System.setProperty("webdriver.chorome.driver", "C:\\Users\\msazz\\eclipse-workspace\\schoolWebsite\\driver\\chromedriver.exe");
-		
-		//2nd way
-		//System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-		//driver = new ChromeDriver();
-			
-		//for firefor driver
-		//System.setProperty("webdriver.chrome.driver", "./driver/geckodriver.exe");
-		//driver = new FirefoxDriver();
-		
-		//for Edge driver
-		//System.setProperty("webdriver.chrome.driver", "./driver/msedgedriver.exe");
-		//driver = new EdgeDriver();
-		
-		//3rd way
-		//we need to add WebDriverManager dependency in the pom.xml file
-		//we don't need any physical driver for webdriver manager
-		//when webdriver manager used, the most updated version of the browser will be initialized.
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		
-		//To specify the browser version, this way we can do it.(unnecessary)
-		//WebDriverManager.chromedriver().driverVersion(129.0.6422.78).setup();
-		//driver = new ChromeDriver();
-		
-		//for firefox
-		//WebDriverManager.firefoxdriver().setup();
-		//driver = new FirefoxDriver();
-		
-		//for edge
-		//WebDriverManager.edgedriver().setup();
-		//driver = new EdgeDriver();
-		
-		
+		configuration = new Configuration();
+		initDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.enthrallit.com/");
+		driver.get(configuration.getProperties(URL));
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));	
+		long pageLoadWait = Long.parseLong(configuration.getProperties(PGELOAD_WAIT));
+		long implicitlyWait =	Long.parseLong(configuration.getProperties(IMPLICITLY_WAIT));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(pageLoadWait));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitlyWait));	
+		initClass();
+	}
+	
+	public void initDriver() {
+	String browserName =	configuration.getProperties(BROWSER);
+
+	
+	switch (browserName) {
+	case CHROME:
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();	
+		break;
+		
+		case FIREFOX:
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();	
+		break;
+		
+		case EDGE:
+		WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
+		break;
+		
+	default:
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();	
+		break;
+	}
+	}
+	
+	public void initClass() {
 		homePage = new HomePage(driver);
 	}
 	
